21418bd7a55e8ad7ccf6f73ff5625e60933fbc4d
akabeko <akabekobeko@gmail.com>
2016-05-10 20:46:34 +0900

    #31 Update a node modules

--- test/lib/icon-generator.test.js
+++ test/lib/icon-generator.test.js
@@ -1,4 +1,4 @@
-import assert from 'power-assert';
+import assert from 'assert';
 import Path from 'path';
 import TestUtil from '../test-util.js';
 import Logger from '../../src/lib/logger.js';
--- test/lib/png-generator.test.js
+++ test/lib/png-generator.test.js
@@ -1,4 +1,4 @@
-import assert from 'power-assert';
+import assert from 'assert';
 import Fs from 'fs';
 import Path from 'path';
 import Del from 'del';
