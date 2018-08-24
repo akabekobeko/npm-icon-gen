akabekobeko/npm-icon-gen

commit 8ed67a98740ca0dcbb3df323bb89c8558624b00f

#89 Fix CLI size specification is not working

src/lib/icon-generator.js
@@ -17,7 +17,7 @@ export default class IconGenerator {
    *
    * @param {String} src     Path of the SVG file.
    * @param {String} dir     Path of the output files directory.
-   * @param {Object} options Options.
+   * @param {CLIOption} options Options from command line.
    * @param {Logger} logger  Logger.
    *
    * @return {Promise} Promise object.
@@ -36,7 +36,7 @@ export default class IconGenerator {
         return
       }
 
-      PngGenerator.generate(svgFilePath, workDir, options.modes, (err, images) => {
+      PngGenerator.generate(svgFilePath, workDir, options, (err, images) => {
         if (err) {
           Del.sync([workDir], {force: true})
           reject(err)

src/lib/icon-generator.test.js
@@ -20,40 +20,40 @@ describe('IconGenerator', () => {
       const modes = ['ico', 'icns', 'favicon']
       let actual = IconGenerator._getRequiredImageSizes(modes)
       let expected = PNGGenerator.getRequiredImageSizes(modes)
-      assert.deepEqual(actual, expected)
+      assert.deepStrictEqual(actual, expected)
     })
 
     it('ico', () => {
       const modes = ['ico']
       const actual = IconGenerator._getRequiredImageSizes(modes)
       const expected = PNGGenerator.getRequiredImageSizes(modes)
-      assert.deepEqual(actual, expected)
+      assert.deepStrictEqual(actual, expected)
     })
 
     it('icns', () => {
       const modes = ['icns']
       const actual = IconGenerator._getRequiredImageSizes(modes)
       const expected = PNGGenerator.getRequiredImageSizes(modes)
-      assert.deepEqual(actual, expected)
+      assert.deepStrictEqual(actual, expected)
     })
 
     it('favicon', () => {
       const modes = ['favicon']
       const actual = IconGenerator._getRequiredImageSizes(modes)
       const expected = PNGGenerator.getRequiredImageSizes(modes)
-      assert.deepEqual(actual, expected)
+      assert.deepStrictEqual(actual, expected)
     })
 
     it('ico: sizes', () => {
       const sizes  = [16, 32]
       const actual = IconGenerator._getRequiredImageSizes(['ico'], {ico: sizes})
-      assert.deepEqual(actual, sizes)
+      assert.deepStrictEqual(actual, sizes)
     })
 
     it('icns: sizes', () => {
       const sizes  = [64, 128]
       const actual = IconGenerator._getRequiredImageSizes(['icns'], {icns: sizes})
-      assert.deepEqual(actual, sizes)
+      assert.deepStrictEqual(actual, sizes)
     })
   })
 })

.travis.yml
@@ -3,4 +3,4 @@ language: node_js
 node_js:
   - "6"
   - "8"
-  - "9"
+  - "10"

src/bin/cli.js
@@ -47,29 +47,21 @@ Generate an icon from the SVG or PNG file.
 
 Options:
 -h, --help    Display this text.
-
 -v, --version Display the version number.
-
 -i, --input   Path of the SVG file or PNG file directory.
-
 -o, --output  Path of the output directory.
-
 -t, --type    Type of the input file.
               'svg' is the SVG file, 'png' is the PNG files directory.
               Allowed values: svg, png
               Default is 'svg'.
-
 -m, --modes   Mode of the output files.
               Allowed values: ico, icns, favicon, all
               Default is 'all'.
-
 -n, --names   Change an output file names for ICO and ICNS.
               ex: 'ico=foo,icns=bar'
               Default is 'app.ico' and 'app.ico'.
-
 -r, --report  Display the process reports.
               Default is disable.
-
 -s, --sizes   List of sizes to include for ICO and ICNS.
               ex: 'ico=[12,24,32],icns=[12,24,64]'
 
@@ -78,9 +70,11 @@ $ icon-gen -i sample.svg -o ./dist -r
 $ icon-gen -i ./images -o ./dist -t png -r
 $ icon-gen -i sample.svg -o ./dist -m ico,favicon -r
 $ icon-gen -i sample.svg -o ./dist -n ico=foo,icns=bar
+$ icon-gen -i sample.svg -o ./dist -s ico=[16,24,32],icns=[16,32,512]
 
 See also:
-https://github.com/akabekobeko/npm-icon-gen`
+https://github.com/akabekobeko/npm-icon-gen
+`
 
 /**
  * Default command line options.
@@ -332,7 +326,7 @@ export default class CLI {
       switch (key) {
         case OUTPUT_MODES.ico:
         case OUTPUT_MODES.icns:
-          sizes[key] = values
+          sizes[key] = values.map((value) => Number(value))
           break
 
         default:

src/bin/cli.test.js
@@ -87,7 +87,7 @@ describe('CLI', () => {
   describe('_parseMode', () => {
     it('Default', () => {
       const modes = CLI._parseMode()
-      assert.deepEqual(modes, ['ico', 'icns', 'favicon'])
+      assert.deepStrictEqual(modes, ['ico', 'icns', 'favicon'])
     })
 
     it('ico, icns, favicon', () => {
@@ -115,22 +115,22 @@ describe('CLI', () => {
   describe('_parseNames', () => {
     it('ico & icns', () => {
       const names = CLI._parseNames('ico=foo,icns=bar')
-      assert.deepEqual({ ico: 'foo', icns: 'bar' }, names)
+      assert.deepStrictEqual({ ico: 'foo', icns: 'bar' }, names)
     })
 
     it('ico', () => {
       const names = CLI._parseNames('ico=foo')
-      assert.deepEqual({ ico: 'foo' }, names)
+      assert.deepStrictEqual({ ico: 'foo' }, names)
     })
 
     it('icns', () => {
       const names = CLI._parseNames('icns=bar')
-      assert.deepEqual({ icns: 'bar' }, names)
+      assert.deepStrictEqual({ icns: 'bar' }, names)
     })
 
     it('Invalid value', () => {
       const names = CLI._parseNames()
-      assert.deepEqual({}, names)
+      assert.deepStrictEqual({}, names)
     })
   })
 
@@ -138,22 +138,22 @@ describe('CLI', () => {
   describe('_parseSizes', () => {
     it('ico & icns', () => {
       const sizes = CLI._parseSizes('ico=[16,24,32],icns=[16,24,64]')
-      assert.deepEqual({ ico: ['16', '24', '32'], icns: ['16', '24', '64'] }, sizes)
+      assert.deepStrictEqual({ ico: [16, 24, 32], icns: [16, 24, 64] }, sizes)
     })
 
     it('ico', () => {
       const sizes = CLI._parseSizes('ico=[16,24,32]')
-      assert.deepEqual({ ico: ['16', '24', '32'] }, sizes)
+      assert.deepStrictEqual({ ico: [16, 24, 32] }, sizes)
     })
 
     it('icns', () => {
       const sizes = CLI._parseSizes('icns=[16,24,64]')
-      assert.deepEqual({ icns: ['16', '24', '64'] }, sizes)
+      assert.deepStrictEqual({ icns: [16, 24, 64] }, sizes)
     })
 
     it('Invalid value', () => {
       const sizes = CLI._parseSizes()
-      assert.deepEqual({}, sizes)
+      assert.deepStrictEqual({}, sizes)
     })
   })
 })

src/lib/icns-generator.js
@@ -1,7 +1,7 @@
 import Fs from 'fs'
 import Path from 'path'
 import Util from './util.js'
-import {PNG} from 'pngjs-nozlib'
+import { PNG } from 'pngjs-nozlib'
 import RLE from './rle.js'
 import { Buffer } from 'buffer'
 import { Promise } from 'es6-promise'
@@ -89,7 +89,7 @@ export default class ICNSGenerator {
   /**
    * Get the size of the required PNG.
    *
-   * @return {Array.<Number>} Sizes.
+   * @return {Number[]} Sizes.
    */
   static getRequiredImageSizes () {
     return REQUIRED_IMAGE_SIZES

src/lib/png-generator.js
@@ -6,19 +6,43 @@ import ICNSGenerator from './icns-generator.js'
 import ICOGenerator from './ico-generator.js'
 
 /**
+ * Filter the sizes.
+ *
+ * @param {Number[]} sizes Original sizes.
+ * @param {Number[]} filterSizes Filter sizes.
+ *
+ * @return {NUmber[]} Filterd sizes.
+ */
+const filterSizes = (sizes = [], filterSizes = []) => {
+  if (filterSizes.length === 0) {
+    return sizes
+  }
+
+  return sizes.filter((size) => {
+    for (let filterSize of filterSizes) {
+      if (size === filterSize) {
+        return true
+      }
+    }
+
+    return false
+  })
+}
+
+/**
  * Generate the PNG files = require(SVG file.
  */
 export default class PNGGenerator {
   /**
    * Generate the PNG files = require(the SVG file.
    *
-   * @param {String}         src    SVG file path.
-   * @param {String}         dir    Output destination The path of directory.
-   * @param {Array.<String>} modes  Modes of an output files.
-   * @param {Function}       cb     Callback function.
-   * @param {Logger}         logger Logger.
+   * @param {String} src SVG file path.
+   * @param {String} dir Output destination The path of directory.
+   * @param {CLIOption} options Options from command line.
+   * @param {Function} cb Callback function.
+   * @param {Logger} logger Logger.
    */
-  static generate (src, dir, modes, cb, logger) {
+  static generate (src, dir, options, cb, logger) {
     Fs.readFile(src, (err, svg) => {
       if (err) {
         cb(err)
@@ -27,7 +51,7 @@ export default class PNGGenerator {
 
       logger.log('SVG to PNG:')
 
-      const sizes = PNGGenerator.getRequiredImageSizes(modes)
+      const sizes = PNGGenerator.getRequiredImageSizes(options)
       Promise
         .all(sizes.map((size) => {
           return PNGGenerator._generatePNG(svg, size, dir, logger)
@@ -44,21 +68,21 @@ export default class PNGGenerator {
   /**
    * Gets the size of the images needed to create an icon.
    *
-   * @param {Array.<String>} modes Modes of an output files.
+   * @param {CLIOption} options Options from command line.
    *
-   * @return {Array.<Number>} The sizes of the image.
+   * @return {Number[]} The sizes of the image.
    */
-  static getRequiredImageSizes (modes) {
+  static getRequiredImageSizes (options = {}) {
     let sizes = []
-    if (modes && 0 < modes.length) {
-      modes.forEach((mode) => {
+    if (options.modes && 0 < options.modes.length) {
+      options.modes.forEach((mode) => {
         switch (mode) {
           case 'icns':
-            sizes = sizes.concat(ICNSGenerator.getRequiredImageSizes())
+            sizes = sizes.concat(filterSizes(ICNSGenerator.getRequiredImageSizes(), options.sizes && options.sizes.icns))
             break
 
           case 'ico':
-            sizes = sizes.concat(ICOGenerator.getRequiredImageSizes())
+            sizes = sizes.concat(filterSizes(ICOGenerator.getRequiredImageSizes(), options.sizes && options.sizes.ico))
             break
 
           case 'favicon':
@@ -73,7 +97,9 @@ export default class PNGGenerator {
 
     // 'all' mode
     if (sizes.length === 0) {
-      sizes = FaviconGenerator.getRequiredImageSizes().concat(ICNSGenerator.getRequiredImageSizes().concat(ICOGenerator.getRequiredImageSizes()))
+      sizes = FaviconGenerator.getRequiredImageSizes()
+      sizes = sizes.concat(filterSizes(ICNSGenerator.getRequiredImageSizes(), options.sizes && options.sizes.icns))
+      sizes = sizes.concat(filterSizes(ICOGenerator.getRequiredImageSizes(), options.sizes && options.sizes.ico))
     }
 
     return sizes
@@ -89,9 +115,9 @@ export default class PNGGenerator {
   /**
    * Generate the PNG file = require(the SVG data.
    *
-   * @param {Buffer} svg    SVG data that has been parse by svg2png.
-   * @param {Number} size   The size (width/height) of the image.
-   * @param {String} dir    Path of the file output directory.
+   * @param {Buffer} svg SVG data that has been parse by svg2png.
+   * @param {Number} size The size (width/height) of the image.
+   * @param {String} dir Path of the file output directory.
    * @param {Logger} logger Logger.
    *
    * @return {Promise} Image generation task.

src/lib/png-generator.test.js
@@ -7,26 +7,55 @@ import FaviconGenerator from './favicon-generator.js'
 import ICNSGenerator from './icns-generator.js'
 import ICOGenerator from './ico-generator.js'
 import Util from './util.js'
+import Rewire from 'rewire'
 
 /** @test {PNGGenerator} */
 describe('PNGGenerator', () => {
+  const Module = Rewire('./png-generator.js')
+
   /** @test {PNGGenerator#getRequiredImageSizes} */
-  it('getRequiredImageSizes', () => {
-    let expected = PNGGenerator.getRequiredImageSizes()
-    let actual   = [16, 24, 32, 48, 57, 64, 72, 96, 120, 128, 144, 152, 195, 228, 256, 512, 1024]
-    assert.deepEqual(expected, actual)
+  describe('getRequiredImageSizes', () => {
+    it('All', () => {
+      let actual = PNGGenerator.getRequiredImageSizes()
+      let expected = [16, 24, 32, 48, 57, 64, 72, 96, 120, 128, 144, 152, 195, 228, 256, 512, 1024]
+      assert.deepStrictEqual(actual, expected)
+    })
 
-    expected = PNGGenerator.getRequiredImageSizes(['ico'])
-    actual   = ICOGenerator.getRequiredImageSizes()
-    assert.deepEqual(expected, actual)
+    it('All with filter', () => {
+      let actual = PNGGenerator.getRequiredImageSizes({ sizes: { ico: [256], icns: [512] } })
+      let expected = [16, 24, 32, 48, 57, 64, 72, 96, 120, 128, 144, 152, 195, 228, 256, 512]
+      assert.deepStrictEqual(actual, expected)
+    })
 
-    expected = PNGGenerator.getRequiredImageSizes(['icns'])
-    actual   = ICNSGenerator.getRequiredImageSizes()
-    assert.deepEqual(expected, actual)
+    it('ICO', () => {
+      let actual = PNGGenerator.getRequiredImageSizes({ modes: ['ico'] })
+      let expected = ICOGenerator.getRequiredImageSizes()
+      assert.deepStrictEqual(actual, expected)
+    })
 
-    expected = PNGGenerator.getRequiredImageSizes(['favicon'])
-    actual   = FaviconGenerator.getRequiredImageSizes()
-    assert.deepEqual(expected, actual)
+    it('ICO with filter', () => {
+      let actual = PNGGenerator.getRequiredImageSizes({ modes: ['ico'], sizes: { ico: [16, 32, 64] } })
+      let expected = [16, 32, 64]
+      assert.deepStrictEqual(actual, expected)
+    })
+
+    it('ICNS', () => {
+      let actual = PNGGenerator.getRequiredImageSizes({ modes: ['icns'] })
+      let expected = ICNSGenerator.getRequiredImageSizes()
+      assert.deepStrictEqual(actual, expected)
+    })
+
+    it('ICNS with filter', () => {
+      let actual = PNGGenerator.getRequiredImageSizes({ modes: ['icns'], sizes: { icns: [16, 1024] } })
+      let expected = [16, 1024]
+      assert.deepStrictEqual(actual, expected)
+    })
+
+    it('Favicon', () => {
+      let actual = PNGGenerator.getRequiredImageSizes({ modes: ['favicon'] })
+      let expected = FaviconGenerator.getRequiredImageSizes()
+      assert.deepStrictEqual(actual, expected)
+    })
   })
 
   /** @test {PNGGenerator#_generetePNG} */
@@ -49,5 +78,21 @@ describe('PNGGenerator', () => {
         assert()
         Del.sync([dir], {force: true})
       })
+  })
+
+  describe('filterSizes', () => {
+    const filterSizes = Module.__get__('filterSizes')
+
+    it('Filter', () => {
+      const actual = filterSizes(ICNSGenerator.getRequiredImageSizes(), [16, 18, 32, 64])
+      const expected = [16, 32, 64]
+      assert.deepStrictEqual(actual, expected)
+    })
+
+    it('No filter', () => {
+      const actual = filterSizes(ICNSGenerator.getRequiredImageSizes())
+      const expected = [16, 32, 64, 128, 256, 512, 1024]
+      assert.deepStrictEqual(actual, expected)
+    })
   })
 })

commit 062ba1e8b6217937fb00be51acaf6ca180a66af4

Fix regression introduced with 8ed67a9

src/lib/icon-generator.js
@@ -69,7 +69,7 @@ export default class IconGenerator {
       logger.log('  src: ' + pngDirPath)
       logger.log('  dir: ' + destDirPath)
 
-      const images = IconGenerator._getRequiredImageSizes(options.modes, options.sizes)
+      const images = IconGenerator._getRequiredImageSizes(options)
         .map((size) => {
           return Path.join(pngDirPath, size + '.png')
         })
@@ -100,19 +100,19 @@ export default class IconGenerator {
     })
   }
 
-  static _getRequiredImageSizes (modes, sizes) {
-    if (!(sizes)) {
-      return PngGenerator.getRequiredImageSizes(modes)
+  static _getRequiredImageSizes (options) {
+    if (!(options.sizes)) {
+      return PngGenerator.getRequiredImageSizes(options)
     }
 
     let imageSizes = []
-    modes.forEach((mode) => {
-      if (sizes[mode]) {
-        imageSizes = imageSizes.concat(sizes[mode])
+    options.modes.forEach((mode) => {
+      if (options.sizes[mode]) {
+        imageSizes = imageSizes.concat(options.sizes[mode])
       }
     })
 
-    return 0 < imageSizes.length ? imageSizes : PngGenerator.getRequiredImageSizes(modes)
+    return 0 < imageSizes.length ? imageSizes : PngGenerator.getRequiredImageSizes(options)
   }
 
   /**

src/lib/icon-generator.test.js
@@ -18,41 +18,41 @@ describe('IconGenerator', () => {
   describe('_getRequiredImageSizes', () => {
     it('ico, icns, favicon', () => {
       const modes = ['ico', 'icns', 'favicon']
-      let actual = IconGenerator._getRequiredImageSizes(modes)
-      let expected = PNGGenerator.getRequiredImageSizes(modes)
+      let actual = IconGenerator._getRequiredImageSizes({modes: modes})
+      let expected = PNGGenerator.getRequiredImageSizes({modes: modes})
       assert.deepStrictEqual(actual, expected)
     })
 
     it('ico', () => {
       const modes = ['ico']
-      const actual = IconGenerator._getRequiredImageSizes(modes)
-      const expected = PNGGenerator.getRequiredImageSizes(modes)
+      const actual = IconGenerator._getRequiredImageSizes({modes: modes})
+      const expected = PNGGenerator.getRequiredImageSizes({modes: modes})
       assert.deepStrictEqual(actual, expected)
     })
 
     it('icns', () => {
       const modes = ['icns']
-      const actual = IconGenerator._getRequiredImageSizes(modes)
-      const expected = PNGGenerator.getRequiredImageSizes(modes)
+      const actual = IconGenerator._getRequiredImageSizes({modes: modes})
+      const expected = PNGGenerator.getRequiredImageSizes({modes: modes})
       assert.deepStrictEqual(actual, expected)
     })
 
     it('favicon', () => {
       const modes = ['favicon']
-      const actual = IconGenerator._getRequiredImageSizes(modes)
-      const expected = PNGGenerator.getRequiredImageSizes(modes)
+      const actual = IconGenerator._getRequiredImageSizes({modes: modes})
+      const expected = PNGGenerator.getRequiredImageSizes({modes: modes})
       assert.deepStrictEqual(actual, expected)
     })
 
     it('ico: sizes', () => {
       const sizes  = [16, 32]
-      const actual = IconGenerator._getRequiredImageSizes(['ico'], {ico: sizes})
+      const actual = IconGenerator._getRequiredImageSizes({modes: ['ico'], sizes: {ico: sizes}})
       assert.deepStrictEqual(actual, sizes)
     })
 
     it('icns: sizes', () => {
       const sizes  = [64, 128]
-      const actual = IconGenerator._getRequiredImageSizes(['icns'], {icns: sizes})
+      const actual = IconGenerator._getRequiredImageSizes({modes: ['icns'], sizes: {icns: sizes}})
       assert.deepStrictEqual(actual, sizes)
     })
   })