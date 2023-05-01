57e90919dcecfe2551dd621b5ad3499f73641f40
akabeko <akabekobeko@gmail.com>
2023-05-01 17:34:15 +0900

    fix: New API

--- src/lib/png.test.ts
+++ src/lib/png.test.ts
@@ -19,11 +19,11 @@ describe('PNG', () => {
       return generatePNG('./examples/data/sample.svg', dir, [16], new Logger())
         .then((results) => {
           assert(results[0].size === 16)
-          del.sync([dir], { force: true })
+          del.deleteSync([dir], { force: true })
         })
         .catch((err) => {
           console.error(err)
-          del.sync([dir], { force: true })
+          del.deleteSync([dir], { force: true })
         })
     })
   })
